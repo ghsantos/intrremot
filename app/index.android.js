7476a988bae570f579f815e9c79f50da84daa9cc
GustavoSantos <ghsantos933@gmail.com>
2017-09-30 02:07:12 -0300

    rename components dir

--- app/index.android.js
+++ app/index.android.js
@@ -4,8 +4,8 @@ import {
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
-import HomeScreen from './telas/home';
-import LedsControl from './telas/control'
+import HomeScreen from './components/home';
+import LedsControl from './components/control'
 
 const Intrremot = StackNavigator ({
   home: { screen: HomeScreen },
