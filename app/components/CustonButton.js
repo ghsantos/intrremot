676f36afd445d55365f3862d67515991c7350cfa
GustavoSantos <ghsantos933@gmail.com>
2017-11-26 20:33:03 -0200

    updated CustonButton

--- app/components/CustonButton.js
+++ app/components/CustonButton.js
@@ -28,8 +28,10 @@ export default class CustonButton extends Component {
     return (
       <TouchableOpacity onPress={() => this.onPressButton()}>
         <View>
-          <View style={this.state.buttonOn ? styles.containerOn : styles.containerOff}>
-            {/*<View style={styles.ledStatus}></View>*/}
+          <View
+            style={[styles.container,
+            this.state.buttonOn ? styles.containerOn : styles.containerOff]}
+          >
             <Text style={styles.text}>{this.props.title}</Text>
             <Text style={styles.text}>{this.state.buttonOn ? 'ON' : 'OFF'}</Text>
           </View>
@@ -46,10 +48,8 @@ CustonButton.propTypes = {
 };
 
 const styles = StyleSheet.create({
-  containerOn: {
+  container: {
     margin: 10,
-    backgroundColor: '#5E35B1',
-    borderColor: '#532e96',
     borderWidth: 6,
     alignItems: 'center',
     justifyContent: 'center',
@@ -57,36 +57,18 @@ const styles = StyleSheet.create({
     height: 200,
     borderRadius: 100,
     elevation: 5,
+  },
+  containerOn: {
+    backgroundColor: '#5E35B1',
+    borderColor: '#532e96',
+  },
+  containerOff: {
+    backgroundColor: '#14082e',
+    borderColor: '#32195e',
   },
   text: {
     color: '#eeeeee',
     fontSize: 30,
     fontWeight: 'bold',
-  },
-  ledStatus: {
-    backgroundColor: '#fff',
-    width: 14,
-    height: 14,
-    borderRadius: 7,
-    marginBottom: 30,
-  },
-  containerOff: {
-    margin: 10,
-    backgroundColor: '#14082e',
-    borderColor: '#32195e',
-    borderWidth: 6,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 200,
-    height: 200,
-    borderRadius: 100,
-    elevation: 5,
-  },
-  ledStatusOff: {
-    backgroundColor: '#302a3a',
-    width: 14,
-    height: 14,
-    borderRadius: 7,
-    marginBottom: 30,
   },
 });
